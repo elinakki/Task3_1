6096f57fe1346dfd16f7e16f189960ef6f5c2dfc
elinakki <n1rael00@students.oamk.fi>
2022-05-07 10:32:12 +0300

    Added comments and some new js functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
+/* Created a button that prints out "Klikkasit buttonia" when pressed */
+
+let button = document.querySelector("#clickme");
+button.onclick = clickButton;
+
 function clickButton() {
     console.log("Klikkasit buttonia");
 }
 
+/* Created a textbox that changes color */
+
 let boxArea = document.getElementById("boxarea");
 
-boxArea.addEventListener("click", doSome)
+boxArea.addEventListener("click", doSome);
 
-function doSome (event) {
-    event.target.classList.add("change");
+function doSome(event) {
+    event.currentTarget.classList.add("change");
+}
+
+/* Manipulated h1 styles */
+
+let h1 = document.querySelector("h1");
+
+h1.classList.add("special");
+h1.style.fontFamily = "Verdana";
+
+/* Added new p element and function for it to change fontcolor when mouse is brought over the text */
+
+let p = document.getElementById("p2");
+
+p.addEventListener("mouseover", changeStyle);
+p.addEventListener("mouseleave", changeStyle);
+
+function changeStyle () {
+    p.classList.toggle("textstyle");
 }
